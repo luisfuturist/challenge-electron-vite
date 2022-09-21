0f90ae32bdbd6b3941e78fb79803de71bc3ab9cb
Luis Float <contact@luisfloat.com>
2022-09-21 14:51:35 -0300

    feat(main): add app header

--- src/main/components/App.js
+++ src/main/components/App.js
@@ -1,10 +1,12 @@
 
 const { BrowserRouter } = require("react-router-dom");
 const { e } = require("../assets/utils.js");
-const routes = require("../routes.js");
+const Header = require("./Header.js");
 
 const template = () => {
-    return e(BrowserRouter, {}, routes);
+    return e(BrowserRouter, {},
+        Header(),
+    );
 };
 
 function App() {
---
+++ src/main/components/Header.js
@@ -0,0 +1,22 @@
+const { Navbar, Container, Nav } = require("react-bootstrap");
+const { e } = require("../assets/utils");
+
+const template = () => {
+    return e("div", null,
+        e(Navbar, { bg: "dark", variant: "dark", expand: "lg", fixed: "top" }, 
+            e(Container, {},
+                e(Navbar.Brand, { href: "#home" }, "OS Info"),
+                e(Navbar.Toggle, { "aria-controls": "main-navbar-nav" }),
+                e(Navbar.Collapse, { id: "main-navbar-nav", variant: "primary" },
+                    e(Nav, { className: "me-auto" }),
+                ),
+            ),
+        ),
+    );
+};
+
+function Header() {
+    return template();
+}
+
+module.exports = Header;
