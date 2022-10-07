7a1d20308e438189ab78c2fad0c0d4bc5d3a27ab
Luis Float <contact@luisfloat.com>
2022-10-06 22:47:07 -0300

    bug(main): update brand name in header component

--- src/main/components/Header.js
+++ src/main/components/Header.js
@@ -5,7 +5,7 @@ const ni = require("../nodes/ni.js");
 const Header = () => {
     return e(Navbar, { bg: "dark", variant: "dark", expand: "lg", fixed: "top" }, 
         e(Container, {},
-            e(Navbar.Brand, { href: "#home" }, "OS Info"),
+            e(Navbar.Brand, { href: "#home" }, "Systemi"),
             e(Navbar.Toggle, { "aria-controls": "main-navbar-nav" }),
             e(Navbar.Collapse, { id: "main-navbar-nav", variant: "primary" },
                 e(Nav, { className: "me-auto" },
