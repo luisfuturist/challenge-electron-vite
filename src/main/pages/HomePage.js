e457a841aecb80170abeabcbe2229d4fcc1cd501
Luis Float <contact@luisfloat.com>
2022-10-06 22:47:57 -0300

    bug(main): fix app name and description in home page

--- src/main/pages/HomePage.js
+++ src/main/pages/HomePage.js
@@ -2,8 +2,8 @@ const { e } = require("../assets/utils.js");
 
 const HomePage = () => {
     return e("div", { className: "text-center" },
-        e("h1", { className: "text-primary" }, "OS Info"),
-        e("p", {}, "OS info desktop app powered by Electron."),
+        e("h1", { className: "text-primary" }, "Systemi"),
+        e("p", {}, "Desktop app for displaying system information."),
         e("hr"),
     );
 };
